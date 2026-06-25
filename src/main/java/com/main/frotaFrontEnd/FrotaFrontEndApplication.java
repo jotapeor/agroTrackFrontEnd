0b287bda09fd6cdfd5d7c5d9442dbfdd663d7134
jotapeor <joaopauloor2004@gmail.com>
2026-06-25 14:03:25 -0300

    -init

---
+++ src/main/java/com/main/frotaFrontEnd/FrotaFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.main.frotaFrontEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrotaFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrotaFrontEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/frotaFrontEnd/FrotaFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.frotaFrontEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrotaFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
